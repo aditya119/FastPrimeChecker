65ef6e6910bca575526d8221ea8913314a371772
Aditya Sharma <aditya119@live.com>
2017-09-10 22:28:27 +0530

    Add files via upload

---
+++ FastPrimeCheck.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool isPrime(int n)
+{
+	if(n == 1)
+		return false;
+	int x = sqrt(n);
+	for(int i = 2; i <= x; i++)
+	{
+		if(n % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	int x;
+	cout << "enter number:\n";
+	cin >> x;
+	cout << "the number is prime: ";
+	if(isPrime(x))
+		cout << "true";
+	else
+		cout << "false";
+	return 0;
+}
